23259cf4419f0e3e6e1fe0a509e55c668b251aaa
Vasanth Kumar <Vasanthkumar30mj@gmail.com>
2021-09-26 19:56:51 +0530

    Arithmetic

---
+++ ArithmeticOps.js
@@ -0,0 +1,40 @@
+var a = 1, b = 2, c = 3;
+console.log(a+b)
+console.log(b-c)
+console.log(a+b+c)
+console.log('----------------')
+a++;
+b++;
+c++;
+console.log(a+b)
+console.log(b-c)
+console.log(a+b+c)
+console.log('----------------')
+a += 5;
+b += 5;
+c += 5;
+console.log(a+b)
+console.log(b-c)
+console.log(a+b+c)
+console.log('----------------')
+a -= 5;
+b -= 5;
+c -= 5;
+console.log(a-b)
+console.log(b-c)
+console.log(a-b-c)
+console.log('----------------')
+a *= 5;
+b *= 5;
+c *= 5;
+console.log(a*b)
+console.log(b*c)
+console.log(a*b*c)
+console.log('----------------')
+a /= 5;
+b /= 5;
+c /= 5;
+console.log(a/b)
+console.log(b/c)
+console.log(a/b/c)
+console.log('----------------')
